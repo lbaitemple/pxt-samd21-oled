810218075a4a8401745bbeb0371adf6a79662602
lbaitemple <lbai@temple.edu>
2023-05-07 10:03:20 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -29,7 +29,7 @@ namespace SSD1306 {
     const SSD1306_COMSCANDEC = 0xC8
     const SSD1306_SEGREMAP = 0xA0
     const SSD1306_CHARGEPUMP = 0x8D
-    const chipAdress = 0x3D
+    const chipAdress = 0x3C
     const xOffset = 0
     const yOffset = 0
     let charX = 0
